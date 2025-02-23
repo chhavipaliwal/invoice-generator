a9d1e7c40293439ed617fa64f9b7435db02afdc0
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-02-23 21:30:39 +0530

    Add download button to invoice preview

--- client/src/components/invoice/preview/index.tsx
+++ client/src/components/invoice/preview/index.tsx
@@ -1,12 +1,17 @@
-import { div } from "framer-motion/client";
 import { useInvoice } from "../context";
 import { Invoice1 } from "../../../templates/invoice-1";
+import { Button } from "@heroui/react";
 export default function Preview() {
   const { formik } = useInvoice();
 
   return (
-    <div
-      dangerouslySetInnerHTML={{ __html: Invoice1({ data: formik.values }) }}
-    />
+    <div className="flex flex-col gap-4">
+      <div
+        dangerouslySetInnerHTML={{ __html: Invoice1({ data: formik.values }) }}
+      />
+      <Button color="primary" size="sm">
+        Download
+      </Button>
+    </div>
   );
 }
