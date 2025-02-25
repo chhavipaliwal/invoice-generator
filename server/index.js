a3ca8b9cb2627e2991f625064dfb03fe02e53398
Chhavi Paliwal <chhavipaliwal1203@gmail.com>
2025-02-25 12:27:50 +0530

    Add CORS and body-parser middleware to server configuration

--- server/index.js
+++ server/index.js
@@ -1,10 +1,26 @@
 const express = require("express");
-const app = express();
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const connectDB = require("./db");
 require("dotenv").config();
 
 const PORT = process.env.PORT;
+
+const app = express();
+
+const corsOptions = {
+  origin: [
+    "https://chhavi-paliwal-invoice-generator.vercel.app/",
+    "http://localhost:5173",
+    "http://localhost:3000",
+  ],
+  method: ["GET", "POST", "PUT", "DELETE"],
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(bodyParser.json({ limit: "50mb" }));
 
 app.get("/", async (req, res) => {
   res.send(await connectDB());
